70342d9b5a6991a121c7a1ea836d14256fe413a4
VladDisco07 <32614194+VladDisco07@users.noreply.github.com>
2018-04-01 21:37:18 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 // The token of your bot - https://discordapp.com/developers/applications/me
-const token = 'NDA5NDQ5NTc1NDI2OTQ5MTI0.DVexJA.t8Y6Ntoi5iOYvVXbpz88ezj7cho';
+const token = 'NDMwMDcyMDAxMTcyODY1MDI1.DaK3jQ.dt3atrv4upv-CpPoQxupza1w4sg';
 
 // The ready event is vital, it means that your bot will only start reacting to information
 // from Discord _after_ ready is emitted
@@ -24,13 +24,14 @@ client.on('guildMemberAdd', member => {
   // Do nothing if the channel wasn't found on this server
   if (!channel) return;
   // Send the message, mentioning the member
-  channel.send(`:japanese_goblin: **Bine ai venit**, ${member}!
-Bun venit pe server-ul Spring-Poienarii!
-Speram sa iti placa aceast server care este facut cu mare grija, pentru a putea sa te simti bine.
-:japanese_goblin: **Ghid noi-veniti**:
-Pentru inceput, te rugam sa treci pe aceste canale:
-- #index - Vezi index-ul si regulamentul server-ului
-- #info - Noutatile acestui server`);
+  channel.send(`** :pushpin: Bun venit ${member} pe WonderGames! :shield: **
+`` Nu uita sa vizitezi ``
+** #aplicatie-staff - Pentru a fi in staff sa faci o cerere!
+#cerere-parteneriat - daca doresti parteneriat cu noi
+#magazin-grade - dacxa doresti sa cumperi un grad!
+#anunturi - anunturile!
+#index - Index-ul server-ului!**
+:tools: ``Bun venit si ms de join`` :wink: :heart:`);
 });
 
 // Log our bot in
